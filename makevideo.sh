28969eb990e633a39e4baaff98ad5918bbcbabc9
adam <adam@kat-dbes.k7dbe.karoo.kat.ac.za>
2012-05-14 12:52:51 +0200

    make video changes

--- makevideo.sh
+++ makevideo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mencoder "mf://data/*.jpg" -mf fps=15 -o output.avi -ovc lavc -lavcopts vcodec=mjpeg:mbd=1:vbitrate=5000 -vf crop=1280:720
+mencoder "mf://data/*.jpg" -mf fps=24 -o output.avi -ovc lavc -lavcopts vcodec=mjpeg:mbd=1:vbitrate=5000 -vf crop=1280:720:0:0,pp=hb/vb/dr/al
