295ff11d59965135e850ce16ad08dce72add03e6
adam <adam@ska.ac.za>
2012-05-16 01:01:25 +0200

    working on production

--- capture.sh
+++ capture.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
 #some cool date arithmetic to get yesterday date -d @$[ $(date +%s) - 60*60*24] +%F 
 #cool trick with find find . -name "*jpg" -mtime -1
 
-PREFIX=$(pwd)
-#PREFIX=/home/adam/asc-webcam-site
+#PREFIX=$(pwd)
+PREFIX=/home/adam/asc-webcam-site
 TODAY=$(date +%F)
 
-if [[ -v $1 ||  ($1 != "day"  && $1 != "night") ]];
+if [[ -v $1 ||  ($1 != "day" && $1 != "night") ]];
 then
   echo "usage: " $0 " [day|night]"
   exit 1
